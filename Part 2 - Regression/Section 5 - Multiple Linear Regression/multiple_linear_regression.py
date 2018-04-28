3f638abd491d5d6b702bacf5feba03f7030abc25
“[mithun <mithunx.manohar@intel.com>
2018-04-29 01:37:14 +0530

    chanes

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 data_set = pd.read_csv("50_Startups.csv")
 
 X = data_set.iloc[:, :-1].values
-y = data_set.iloc[:, -1]
+y = data_set.iloc[:, -1].values
 
 # encode the data
 
@@ -24,17 +24,31 @@ label_encoder = LabelEncoder()
 
 X[:,3] = label_encoder.fit_transform(X[:,3])
 
+
 one_hot_encoder = OneHotEncoder(categorical_features = [3])
 
 X = one_hot_encoder.fit_transform(X).toarray()
 
-# split data
+# avoid dummy trap variable
+
+X = X[:, 1:]
+
+# split data to train and test data
 
 from sklearn.cross_validation import train_test_split
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, 
                                                     random_state = 0)
 
-#andle missing data
 
-# encode data
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+
+regressor.fit(X_train, y_train)
+
+# predicting test result
+
+y_pred = regressor.predict(X_test)
+
+
+
